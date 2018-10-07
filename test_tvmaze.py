6510d407b099f49fad1a721d0c865e4b8d858baa
Roberto Pauletto <roberto.pauletto@gmail.com>
2018-10-07 16:10:22 +0200

    Python 3 porting, some refactoring, some new methods added

--- test_tvmaze.py
+++ test_tvmaze.py
@@ -1,80 +1,100 @@
-import unittest
-import tvmaze
+# test_tv_maze
 
+from unittest import TestCase
+import tvs4.tv_maze.tvmaze as tvm
 
-class MyTestCase(unittest.TestCase):
-    def test_search_show_not_found(self):
-        result = tvmaze.find_shows("a non existent show")
-        self.assertEqual(len(result), 0,  "Should be an empty list!")
+__doc__ = """test_tv_maze"""
+__version__ = "0.2"
+__changelog__ = """
 
-    def test_search_show_found(self):
-        result = tvmaze.find_shows("girls")
-        self.assertIsNotNone(result, "Exptected a populated list")
-
-    def test_singlesearch_show_not_found(self):
-        result = tvmaze.single_search("a non existent show")
-        self.assertIsNone(result, "Should be none, got %s" % type(result))
-
-    def test_singlesearch_show_found(self):
-        result = tvmaze.single_search("walking dead")
-        self.assertIsNotNone(result, "Exptected some show infos")
-
-    def test_get_show_by_id_found(self):
-        result = tvmaze.get_show(73)
-        self.assertTrue('walking dead' in result['name'].lower(),
-                        "The walking dead show should be found")
-
-    def test_get_show_by_id_not_found(self):
-        result = tvmaze.get_show(7121454543)
-        self.assertIsNone(result, "Should be None")
-
-    def test_get_show_id_not_found(self):
-        result = tvmaze.get_show_id("a non existent show")
-        self.assertEqual(result, -1, "Should be -1")
-
-    def test_get_show_id_found(self):
-        result = tvmaze.get_show_id("walking dead")
-        self.assertEqual(result, 73, "Expected 73")
-
-    def test_get_show_and_episodes_short_show_id_not_found(self):
-        show_id, show_name, episodes = tvmaze.get_show_and_episodes_short(
-            tvmaze.get_show_id("a non existent show")
-        )
-        self.assertIsNone(show_name, "If show not found should be None")
-        self.assertEqual(len(episodes), 0, "Should be an empty list")
-
-    def test_get_show_and_episodes_short_show_id_found(self):
-        show_id, show_name, episodes = tvmaze.get_show_and_episodes_short(
-            tvmaze.get_show_id("walking dead")
-        )
-        self.assertTrue('walking dead' in show_name.lower())
-        self.assertGreater(len(episodes), 0, "Expected a populated list")
-
-    def test_get_episodes_show_id_found_selected_seasons(self):
-        show_id, show_name, episodes = tvmaze.get_show_and_episodes_short(
-            tvmaze.get_show_id("walking dead"), (2,)
-        )
-        test = 2
-        for ep in episodes:
-            if ep['season'] != 2:
-                test = ep['season']
-                break
-        self.assertEqual(test, 2, "Expected season 2 only")
-
-    def test_get_number_of_seasons(self):
-        self.assertGreaterEqual(
-            tvmaze.get_seasons_number(tvmaze.get_show_id("buffy")), 7,
-                "The Buffy show has 7 seasons"
-            )
-
-    def test_get_shows_found_something(self):
-        result = tvmaze.get_shows("girls")
-        self.assertGreater(result, 0, "Expected a populated list!")
-
-    def test_get_shows_found_nothing(self):
-        result = tvmaze.get_shows("nonexistent")
-        self.assertEqual(len(result), 0, "Expected a populated list!")
-
+"""
 
 if __name__ == '__main__':
-    unittest.main()
+    pass
+
+
+class TestTvMaze(TestCase):
+
+    def setUp(self):
+        self.show_unique = 'the big bang theory'
+        self.show_multiple = 'girls'
+        self.test_route = r'http://api.tvmaze.com/search/shows'
+
+    def _show_name_all_occurrenes(self, r, tosearch):
+        for item in r:
+            if not tosearch.lower() in item['show']['name'].lower():
+                return False
+        return True
+
+    def _get_unique_show(self):
+        r = tvm.get_shows(self.show_unique)
+        r = tvm.get_show_by_id(r[0][0])
+        return r
+
+    def test_route_key_not_found(self):
+        self.assertEqual(tvm._set_query('not_a_route_key'), None)
+
+    def test_route_key_ok(self):
+        self.assertEqual(tvm._set_query('search'), self.test_route,
+                         f'Should be {self.test_route}')
+
+    def test_get_data_none(self):
+        not_a_show = "asdfsdfewet"
+        self.assertEqual(tvm._get_data(self.test_route, not_a_show), None)
+
+    def test_get_data_ok(self):
+        r = tvm._get_data(self.test_route, {'q': self.show_unique})
+        self.assertTrue(r is not None)
+
+    def test_find_shows(self):
+        r = tvm._get_data(self.test_route, {'q': self.show_multiple})
+        self.assertTrue(len(r) > 1, "Return value should be a list of dicts")
+
+    def test_find_shows_name_expected(self):
+        r = tvm._get_data(self.test_route, {'q': self.show_multiple})
+        self.assertTrue(self._show_name_all_occurrenes(r, self.show_multiple))
+
+    def test_get_shows_ok(self):
+        r = tvm.get_shows(self.show_multiple)
+        print(r)
+        self.assertTrue(len(r) > 1)
+
+    def test_get_show_id_not_found(self):
+        not_a_show = "asdfsdfewet"
+        self.assertTrue(tvm.get_show_id(not_a_show), -1)
+
+    def test_get_show_id_found(self):
+        self.assertTrue(tvm.get_show_id(self.show_unique), 66)
+
+    def test_get_show_by_id_ko(self):
+        not_valid_id = '123123123123123123'
+        r = tvm.get_show_by_id(not_valid_id)
+        self.assertTrue(r is None)
+
+    def test_get_show_by_id_ok(self):
+        r = tvm.get_shows(self.show_unique)
+        r = tvm.get_show_by_id(r[0][0])
+        self.assertTrue(r['name'].lower() == 'the big bang theory')
+
+    def test_get_show_image_medium_ok(self):
+        r = tvm.get_show_image(66, tvm.ImgType.medium)
+        print(r)
+        self.assertTrue(r is not None)
+
+    def test_get_show_status_not_a_show(self):
+        not_valid_id = '123123123123123123'
+        self.assertEqual(tvm.get_show_status(not_valid_id), None)
+
+    def test_get_show_status_ok(self):
+        self.assertTrue(tvm.get_show_status(66).lower() in ('ended', 'running'))
+
+    def test_is_running_show_true(self):
+        """Careful, ckeck if show_id is actually running, otherwise the test
+        will fail"""
+        # 66 = the big bang theory
+        self.assertTrue(tvm.is_a_running_show(66))
+
+    def test_is_running_show_false(self):
+        # 1 = under the dome
+        self.assertFalse(tvm.is_a_running_show(1))
+
--- tvmaze.py
+++ tvmaze.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# tvmaze.py
+
 
 try:
+    import enum
     import requests
     import json
 except ImportError as ie:
-    print "Some module is missing:\n" + ie
+    print("Some modules are missing:\n")
 
 __doc__ = """Python interface to the TV Maze API
 
-This interface returns always JSON objects except for the convenience method
-`get_show_and_episodes_short` which retrieves some info only about shows and
-episodes such as
+This interface always returns JSON objects except for the convenience method
+`get_show_and_episodes_short` which returns some info about shows and
+episodes only such as:
 
 - Show Name
 - Number of seasons
@@ -20,13 +21,13 @@ episodes such as
     - Title
     - Season show
 
-Use single_search if you are pretty sure of the name of the show you are
-looking for (e.g. single_search('buffy')
+Use `single_search()` if you are pretty sure of the name of the show you are
+looking for (e.g. `single_search('buffy')`
 
-Use get_shows if you want to retrieve a number of possible matches to process
-to get the show you actually want; for example
+Use `get_shows()` if you want to retrieve a number of possible 
+matches to process to get the show you actually want, for example:
 
-get_shows('girls') will gives you
+`get_shows('girls')` will gives you
 
 000139 Girls
 000525 Gilmore Girls
@@ -40,9 +41,20 @@ get_shows('girls') will gives you
 013826 Soldier Girls
 
 """
-__version__ = '0.1'
+__version__ = '0.3'
+__changelog__ = """
 
-TVMAZE_API = {
+"""
+
+
+class ImgType(enum.Enum):
+    """Image format"""
+    medium = 'medium'
+    original = 'original'
+
+
+# Routes
+_routes = {
     'root': r'http://api.tvmaze.com/',
     'search': r'search/shows',
     'single_search': r'singlesearch/shows',
@@ -57,25 +69,28 @@ class TVMazeException(Exception):
 
 
 def _set_query(qtype):
-    """(str) -> str
-
+    """
     Compose the REST url to request, according to `qtype`, returns None if
     `qtype` is not found
+
+    :param qtype: key of route
+    :return: the api url for the search specified by `qtype`
     """
-    if qtype not in TVMAZE_API:
+    if qtype not in _routes:
         return None
-    return "%s%s" % (TVMAZE_API['root'], TVMAZE_API[qtype])
+    return f"{_routes['root']}{_routes[qtype]}"
 
 
-def _set_request(url, parms=None):
-    """(str [,dict]) -> JSON string or None
-
-    Execute a REST request with `url` and optionals parameters `parms`
-    Returns None or the JSON object from the request
+def _get_data(url, payload=None):
     """
-    r = None
-    if parms:
-        r = requests.get(url, params=parms)
+    Execute a REST request with `url` and optionals parameters
+
+    :param url: the API url
+    :param payload:
+    :return: None or the JSON object from the request
+    """
+    if payload:
+        r = requests.get(url, params=payload)
     else:
         r = requests.get(url)
     return r.json() if r else None
@@ -92,37 +107,37 @@ def find_shows(query):
 
     """
     url = _set_query('search')
-    parms = {'q': query}
-    return _set_request(url, parms)
+    payload = {'q': query}
+    return _get_data(url, payload)
 
 
 def single_search(query):
-    """(str) -> JSON string or None
-
-    Performs a search for a show with query as name (forgiving for typos).
+    """
+    Performs a search for a show with query as name (forgiving typos).
     Use it if you are sure that the search for `query` will return only one
     show
+
+    :param query:
+    :return:
     """
     url = _set_query('single_search')
-    parms = {'q': query}
-    return _set_request(url, parms)
+    payload = {'q': query}
+    return _get_data(url, payload)
 
 
 def get_shows(query):
-    """(str) -> list of tuples with show id and name
-
+    """
     Discards all the informations and returns a list of show ids and names.
-    It's up to the caller to eventually pick a specific entry.
+    Useful if you want the user to choose a show in case of multiple
+    occurrences
 
-    Returns an empty list if nothing is found
+    :param query:
+    :return: list of tuples with show id and name, empty list if nothing found
     """
     results = find_shows(query)
     if not results:
         return []
-    retval = []
-    for result in results:
-        retval.append((result['show']['id'], result['show']['name']))
-    return retval
+    return [(res['show']['id'], res['show']['name']) for res in results]
 
 
 def get_show_id(query):
@@ -134,37 +149,75 @@ def get_show_id(query):
     return show['id'] if show else -1
 
 
-def get_show(show_id):
-    """Return info about the show with `show_id`"""
+def get_show_by_id(show_id):
+    """Return info about the show with `show_id` (none if not found)"""
     url = _set_query('show_info') + str(show_id)
-    show = _set_request(url)
+    show = _get_data(url)
     return show
 
 
-def get_show_and_episodes_short(show_id, seasons=()):
-    """(int [,seasons) -> (list of tuple)
-
-    `seasons` could be a tuple with the season number for which we want
-    episodes.
-
-    If `show_id` does not exists returns None and an empty list
+def get_show_image(show_id, imgtype=ImgType.medium):
+    """
+    Get the image show url
 
     :param show_id:
-    :param seasons:
+    :param <ImgType> imgtype:
+    :return: the raw response (just in case), the image url
+    """
+    show = get_show_by_id(show_id)
+    url = show['image'][imgtype.name]
+    response = requests.get(url, stream=True)
+    return response.raw, url
+
+def get_show_status(show_id):
+    """
+    Get the status of the show
+
+    :param show_id:
+    :return:
+    """
+    show = get_show_by_id(show_id)
+    return show['status'] if show else None
+
+def is_a_running_show(show_id):
+    """
+    Convenience method to check is the show is currently running
+    :param show_id:
+    :return: False if show ended or show **not found**
+    """
+    show = get_show_by_id(show_id)
+    if not show:
+        return False
+    return show['status'].lower() == 'running'
+
+
+
+
+def get_show_and_episodes_short(show_id, seasons=None):
+    """(int [,seasons) -> (list of tuple)
+
+    If `show_id` does not exists returns -1, None and an empty list
+
+    :param show_id:
+    :param seasons: a tuple with the season number for which we want episodes.
+                    empty means we want all seasons
     :return: list of tuple with:
     - serie_id
     - serie name
-    - dict with basic info for the episodes: name, season and episode number
+    - list of dict with basic info for the episodes:
+        - name
+        - season
+        - episode number
     """
-    show = get_show(show_id)
-    if not show:
+    show = get_show_by_id(show_id)
+    if not show:  # Could not found the show with id received!
         return -1, None, []
     url = _set_query('show_episode_list') % show_id
-    r = _set_request(url)
-    if not r:
+    req = _get_data(url)
+    if not req:  # Something's wrong
         return -1, None, []
     result = []
-    for ep in r:
+    for ep in req:
         if seasons and ep['season'] not in seasons:
             continue
         result.append(
@@ -173,10 +226,10 @@ def get_show_and_episodes_short(show_id, seasons=()):
     return show_id, show['name'], result
 
 
-def get_seasons_number(show_id):
+def get_number_of_seasons(show_id):
     """(int) -> int
 
-    Returns the number of season for the show with id `show_id`
+    Returns the number of seasons for the show with id `show_id`
     """
     show_id, show_name, eps = get_show_and_episodes_short(show_id)
     if not eps:
@@ -185,8 +238,11 @@ def get_seasons_number(show_id):
 
 
 if __name__ == '__main__':
-    test_show = 'big bang theory'
-    show_id = get_show_id(test_show)
-    if show_id:
-        print get_show_and_episodes_short(show_id)
+    imgtest = r'/home/robby/ownCloud/code/python/flask_apps/tvseries-4/tvs4/bo'
+    i = get_show_image(155)
+    #test_show = 'big bang theory'
+    #show_id = get_show_id(test_show)
+    #if show_id:
+    #    print(get_show_and_episodes_short(show_id))
     # print "\n".join([str(item[0]).zfill(6) + " " + item[1] for item in get_shows('girls')])
+
